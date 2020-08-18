8530d62c35e0bb0821ea6566b908a2895f2e41b1
Nico Bauer <Bauer.Nico1998@outlook.de>
2020-08-18 08:51:26 +0200

    Änderung

---
+++ Größere Zahl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Nico Bauer
+/// IS218
+/// AWE
+/// Es sollen zwei beliebige Zahlen eingelesen werden können und anschließend die größere der beiden ermittelt und auf dem Bildschirm ausgegeben werden.
+/// </summary>
+
+namespace Größere_Zahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl1 = 0;
+            double zahl2 = 0;
+
+
+        }
+    }
+}
